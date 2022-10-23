32bc02e2c6d0145b35e993a92fdc897adf28091f
hiroki.tanimura <tanitani_526@yahoo.co.jp>
2022-10-23 17:08:05 +0900

    Initial Commit

---
+++ SecondkadaiApp3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondkadaiApp3
+//
+//  Created by 谷村大樹 on 2022/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
